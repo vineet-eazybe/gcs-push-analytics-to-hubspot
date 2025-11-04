9b29fa55411734b26855bc99fc2cb6cde2d44792
vineet-eazybe <vineet@eazybe.com>
2025-11-04 12:02:57 +0530

    Refactor logWebhookSiteConfig to use a constant for the webhook URL and export the function for external use. Comment out the response logging for cleaner output.

--- webhookSiteConfig.js
+++ webhookSiteConfig.js
@@ -1,17 +1,18 @@
 const axios = require('axios');
 
+const webhookSiteUrl = 'https://webhook.site/analytics-to-crm';
 
 logWebhookSiteConfig = async (data) => {
     try {
-        const response = await axios.post('https://webhook.site/analytics-to-crm', {
+        const response = await axios.post(webhookSiteUrl, {
             data: data
         });
-        console.log(response.data);
+        // console.log(response.data);
     } catch (error) {
         console.error('Error in logWebhookSiteConfig:', error);
     }
 }
 
-logWebhookSiteConfig({
-    message: 'Hello, world!'
-});
+module.exports = {
+    logWebhookSiteConfig
+}
